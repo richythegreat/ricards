3289504033f9bdd43c4ea23c503c8c25274cef02
ricards <your@email.com>
2024-09-06 09:20:30 +0300

    select for certain fields, custom field + distinct

--- select/1.sql
+++ select/1.sql
@@ -1,5 +1,8 @@
 -- use sql_store;
 -- select 27*9, "result";
 
--- use sql_invoicing;
-select * from clients;
+use sql_invoicing;
+
+select * from invoices
+where payment_total > 0
+order by client_id;
---
+++ select/2.sql
@@ -0,0 +1,12 @@
+-- uzdevums atlasit datus no tabulas'customers'
+-- datubāze 'sql_store'
+-- atlasit vārdu, uzvārdu un punktus
+-- use sql_store;
+-- select first_name, 
+-- last_name, 
+-- points, 
+-- points+10 as "points increased" 
+-- from customers;
+
+select state from customers;
+SELECT DISTINCT state FROM Customers;
