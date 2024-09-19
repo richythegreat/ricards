fd6ca2d628bdd5c70a4cc0c6e08298a8dbfd77cd
ricards <your@email.com>
2024-09-19 10:12:21 +0300

    insert 1 or multiple rows; hierachital 1

---
+++ 2sql/2sql.sql
@@ -0,0 +1,21 @@
+use sql_store;
+
+insert into
+ customers (
+first_name,
+last_name,
+city,
+state)
+insert into customers(
+first_name,
+last_name,
+address,
+city,
+state)
+values(
+'antra'
+'Berzina',
+'brivibas iela',
+'Riga',
+
+)
---
+++ insert/1.sql
@@ -0,0 +1,14 @@
+-- use sql_store;
+
+-- insert into customers
+-- values(default, 'janis',
+-- 'berzins',
+-- null,
+-- '371 222222',
+-- 'adrese',
+-- 'ventspils',
+-- 'ventspils novads',
+-- 'CA',
+-- default)
+
+select * from customers
